0ce92c706e1bb90470f3f149d42dd61272bdd10f
Piraisudan Babu <piraisudan.babu@solitontech.local>
2025-01-03 15:59:23 +0530

    Add:nth even fibonacci series

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* FibonacciSeries(int n){
+    int firstNum = -1, secondNum = 1, newTwo;
+    
+    int* array = (int*)malloc(n*sizeof(int));
+    
+    for(int i=0; i<n; i++){
+        newTwo = firstNum+secondNum;
+        array[i] = newTwo;
+        firstNum = secondNum, secondNum = newTwo;
+    }
+    
+    return array;
+}
+
 int* FibonacciSeriesEvenNumbers(int* arr, int n){
     int i,size=0;
     for(i=0;i<n;i++){
@@ -11,4 +28,15 @@ int* FibonacciSeriesEvenNumbers(int* arr, int n){
         } 
     }
     return even;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int* array = FibonacciSeries(n);
+    for(int i=0; i<n; i++) printf("%d ", array[i]);
+    printf("\n");
+    int* even=FibonacciSeriesEvenNumbers(array,n);
+    for(int i=0; i<sizeof(even)/sizeof(even[0]); i++) printf("%d ", even[i]);
 }
